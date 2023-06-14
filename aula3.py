54c1a192e5b930ae174164af6632933088afc455
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-13 22:30:07 -0300

    :books: docs: Tipos de dados em Python str

---
+++ aula3.py
@@ -0,0 +1,20 @@
+'''
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro e aspas
+'''
+
+print(1234)
+
+# Aspas simples
+print('Ícaro Nogueira')
+
+# Aspas duplas
+print("Ícaro Lemos")
+print("Ícaro 'Lemos'")
+
+# Escape
+print("Ícaro \"Lemos\"")
+
+# r
+print(r"Ícaro \"Lemos\"")
