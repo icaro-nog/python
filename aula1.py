4936e4b722cae2232663d7468268af0dc461704f
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-13 21:33:53 -0300

    :books: docs: Aula sobre comentários e DocString

---
+++ aula1.py
@@ -0,0 +1,21 @@
+"""
+DocString (!= de comentário)
+
+escreva 
+escreva
+escreva
+"""
+
+print(456)
+
+'''
+DocString (!= de comentário)
+
+escreva 
+escreva
+escreva
+'''
+
+# Comentário
+print(123) # comentário posterior
+# Comentário abaixo
