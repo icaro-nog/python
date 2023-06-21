1fdf4b09637c6d2960c043196354f0a19a19e662
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-20 23:47:38 -0300

    :books: docs: Exercícios com variáveis

---
+++ aula8.py
@@ -0,0 +1,22 @@
+nome = "Harry"
+sobrenome = "Potter de Oliveira"
+idade = 29
+ano_nascimento = 2022 - idade
+
+if idade >= 18:
+    maior_de_idade = 'sim'
+else:
+    maior_de_idade = 'não'
+
+# Ou 
+# maior_de_idade = idade >= 18
+# Retorna true ou false
+
+altura_metros = 9.72
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
