ca1714a9e912a0252cb08778c7bffb95d756114c
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-28 23:11:36 -0300

    :books: docs: Comentários adicionais

--- aula14.py
+++ aula14.py
@@ -1,11 +1,11 @@
 a = 'AAAAAAA'
 b = 'BBBBBBBBB'
 c = 1.1
-string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+string = 'primeira={nome2} segunda={nome1} terceira={nome1} c={nome3:.2f}'
 formato = string.format(
-    nome1=a,
-    nome2=b,
-    nome3=c
+    nome1=a, # {nome1}
+    nome2=b, # {nome2}
+    nome3=c  # {nome3}
 )
 
 print(formato)
---
+++ aula16.py
@@ -0,0 +1,6 @@
+# nome = input('Qual seu nome?')
+# print(f'seu nome é {nome}')
+
+# print(f'seu nome é {nome=}') # Útil para debugar
+
+numero_1 = input('Digite um número: ')
