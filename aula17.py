9b986f2920c331dbf403309f25b7a81e036bcafa
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-30 00:34:15 -0300

    :books: docs: Fluxo condicional

---
+++ aula17.py
@@ -0,0 +1,19 @@
+# if / elif / else
+condicao = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao:
+    print('Este código do if')
+elif condicao2:
+    print('Código para condicao2')
+elif condicao3:
+    print('Código para condicao3')
+elif condicao4:
+    print('Código para condicao4')
+else:
+    print('Nenhuma condição foi satisfeita')
+
+if 10 == 10:
+    print('If do 10')
