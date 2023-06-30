57c0f96e34fa6435794a515a7a4f8fa3fef5febf
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-29 23:50:40 -0300

    :books: docs: Input para coletar dados do usuário

--- aula16.py
+++ aula16.py
@@ -4,3 +4,9 @@
 # print(f'seu nome é {nome=}') # Útil para debugar
 
 numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
