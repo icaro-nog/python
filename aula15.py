e087bac15aedd40f53189a8c046409a7bd9c2f79
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-06-30 00:22:16 -0300

    :books: docs: Introdução a if / elif / else

---
+++ aula15.py
@@ -0,0 +1,9 @@
+# if / elif / else
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você não entrou no sistema')
+else: 
+    print('Você não digitou a resposta esperada')
