8dc1bb40c5933ebc1f4995ba9150267234661f48
Ícaro Nogueira <icaro.nogueira@brudam.com.br>
2023-07-04 23:59:02 -0300

    :books: docs: operador not, not in e iterações com strings

---
+++ aula24.py
@@ -0,0 +1,10 @@
+# Operadores in e not in 
+# String em Python são iteráveis
+# 0 1 2 3 4 5 6
+# í c a r o
+nome = 'ícaro'
+print(nome[1])
+print('í' in nome)
+print('b' in nome)
+print('ro' in nome)
+print('ro' not in nome)
